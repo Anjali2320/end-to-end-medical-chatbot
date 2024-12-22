f59077a745e03f2101311028c8e47e6fb7c2d995
AnjaliBhardwaj16 <anjalibhardwaj@loyalistcollege.com>
2024-12-22 17:23:09 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Anjali Bhardwaj',
+    author_email = "anjalibhardwaj2320@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
